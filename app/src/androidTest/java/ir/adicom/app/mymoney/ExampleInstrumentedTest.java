6afee0c744bbdcf7c6b84375fbe2669b43f0a02c
Adicom.ir <adicom.ir@gmail.com>
2018-08-27 23:25:36 +0430

    Init commit

---
+++ app/src/androidTest/java/ir/adicom/app/mymoney/ExampleInstrumentedTest.java
@@ -0,0 +1,30 @@
+package ir.adicom.app.mymoney;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    // Looks for an EditText with id = "R.id.etInput"
+    // Types the text "Hello" into the EditText
+    // Verifies the EditText has text "Hello"
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.et_input)).perform(typeText("Hello")).check(matches(withText("Hello")));
+    }
+}
