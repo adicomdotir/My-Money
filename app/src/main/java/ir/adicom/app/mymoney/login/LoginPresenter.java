8618f94b7c1424f4061644dbb5e33a0b87bd36a3
Adicom.ir <adicom.ir@gmail.com>
2018-08-29 00:55:45 +0430

    add login logic to presenter

--- app/src/main/java/ir/adicom/app/mymoney/login/LoginPresenter.java
+++ app/src/main/java/ir/adicom/app/mymoney/login/LoginPresenter.java
@@ -1,5 +1,10 @@
 package ir.adicom.app.mymoney.login;
 
+import android.view.View;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * Created by Y.P on 28/08/2018.
@@ -9,5 +14,44 @@ public class LoginPresenter implements LoginContract.Presenter {
     @Override
     public void start() {
 
+    }
+
+    public void checkLogin(View arg0) {
+
+//        final String email = emailEditText.getText().toString();
+//        if (!isValidEmail(email)) {
+//            //Set error message for email field
+//            emailEditText.setError("Invalid Email");
+//        }
+//
+//        final String pass = passEditText.getText().toString();
+//        if (!isValidPassword(pass)) {
+//            //Set error message for password field
+//            passEditText.setError("Password cannot be empty");
+//        }
+//
+//        if(isValidEmail(email) && isValidPassword(pass))
+//        {
+//            // Validation Completed
+//        }
+
+    }
+
+    // validating email id
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    // validating password
+    private boolean isValidPassword(String pass) {
+        if (pass != null && pass.length() >= 4) {
+            return true;
+        }
+        return false;
     }
 }
