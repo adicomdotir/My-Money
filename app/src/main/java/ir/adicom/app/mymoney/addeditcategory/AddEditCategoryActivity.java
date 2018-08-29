f490d18983a7ecaee68d9d212a555a3baf7be612
Adicom.ir <adicom.ir@gmail.com>
2018-08-29 23:56:51 +0430

    Add other screen just activity and fragment

---
+++ app/src/main/java/ir/adicom/app/mymoney/addeditcategory/AddEditCategoryActivity.java
@@ -0,0 +1,15 @@
+package ir.adicom.app.mymoney.addeditcategory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ir.adicom.app.mymoney.R;
+
+public class AddEditCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_category);
+    }
+}
---
+++ app/src/main/java/ir/adicom/app/mymoney/categories/CategoriesActivity.java
@@ -0,0 +1,15 @@
+package ir.adicom.app.mymoney.categories;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ir.adicom.app.mymoney.R;
+
+public class CategoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+}
