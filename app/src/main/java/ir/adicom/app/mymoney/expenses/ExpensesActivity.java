0d06981da0424acb5b4d52cb577e0c9779f1852f
adicomdotir <adicom.ir@gmail.com>
2018-12-12 12:00:24 +0330

    Modify navigation drawer ui

--- app/src/main/java/ir/adicom/app/mymoney/expenses/ExpensesActivity.java
+++ app/src/main/java/ir/adicom/app/mymoney/expenses/ExpensesActivity.java
@@ -38,9 +38,9 @@ public class ExpensesActivity extends AppCompatActivity {
         // Set up the toolbar.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ActionBar ab = getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-        ab.setDisplayHomeAsUpEnabled(true);
+//        ActionBar ab = getSupportActionBar();
+//        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+//        ab.setDisplayHomeAsUpEnabled(true);
 
         // Set up the navigation drawer.
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
