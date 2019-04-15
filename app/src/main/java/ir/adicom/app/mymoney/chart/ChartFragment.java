0a590ef7fdb0d30e50e18edbc9ee75eaa56af1d9
adicomdotir <adicom.ir@gmail.com>
2019-04-15 13:12:50 +0430

    Fix bugs&issue01

--- app/src/main/java/ir/adicom/app/mymoney/chart/ChartFragment.java
+++ app/src/main/java/ir/adicom/app/mymoney/chart/ChartFragment.java
@@ -15,9 +15,11 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import ir.adicom.app.mymoney.R;
 
@@ -60,9 +62,13 @@ public class ChartFragment extends Fragment implements ChartContract.View {
             sumPrice += entry.getValue();
             //entries.add(new PieEntry(entry.getValue(), entry.getKey()));
         }
+        DecimalFormat df = new DecimalFormat("#.#");
+        List<Integer> colors = new ArrayList<>();
         for (Map.Entry<Long, Long> entry : result.entrySet()) {
-            Long percent = entry.getValue() * 100 / sumPrice;
+            float percent = entry.getValue() * 100 / sumPrice;
+            percent = Float.parseFloat(df.format(percent));
             entries.add(new PieEntry(percent, mPresenter.getCategory(entry.getKey())));
+            colors.add(generateDarkColor());
         }
 //        entries.add(new PieEntry(5, "غذا"));
 //        entries.add(new PieEntry(10, "قلیان"));
@@ -70,27 +76,16 @@ public class ChartFragment extends Fragment implements ChartContract.View {
 
         PieDataSet dataSet = new PieDataSet(entries, "دسته ها");
         dataSet.setValueFormatter(new PercentFormatter());
-
-        int[] colors = new int[]{
-                Color.parseColor("#488f31"),
-                Color.parseColor("#3d9c73"),
-                Color.parseColor("#63b179"),
-                Color.parseColor("#88c580"),
-                Color.parseColor("#aed987"),
-                Color.parseColor("#d6ec91"),
-                Color.parseColor("#ffff9d"),
-                Color.parseColor("#fee17e"),
-                Color.parseColor("#fcc267"),
-                Color.parseColor("#f7a258"),
-                Color.parseColor("#ef8250"),
-                Color.parseColor("#e4604e"),
-                Color.parseColor("#de425b")
-        };
         dataSet.setColors(colors);
         dataSet.setValueTextColor(Color.WHITE);
 
         PieData barData = new PieData(dataSet);
         chart.setData(barData);
         chart.invalidate();
+    }
+
+    private Integer generateDarkColor() {
+        Random r = new Random();
+        return Color.rgb(r.nextInt(100), r.nextInt(100), r.nextInt(100));
     }
 }
